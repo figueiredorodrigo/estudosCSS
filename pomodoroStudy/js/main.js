10b085136558ca2dee5a77927c5f8b96ad681e40
Rodrigo Figueiredo <rodrigo84@outlook.com>
2022-12-28 15:21:28 -0300

    finish pomodoro

--- pomodoroStudy/js/main.js
+++ pomodoroStudy/js/main.js
@@ -20,11 +20,11 @@ let minutes = document.querySelector('.minutes');
 let sec = document.querySelector('.seconds');
 let seconds;
 
+const btnStart = document.querySelector('.btn-restart');
 const input = document.querySelector('.input-container');
 const timer = document.querySelector('.timer');
-
 const finalPomodoro = document.querySelector('.final-container');
-
+const closeModal = document.querySelector('.close');
 
 //Audios
 let begin = new Audio("./assets/start.mp3");
@@ -32,17 +32,53 @@ let back = new Audio("./assets/back.mp3");
 let final = new Audio("./assets/final.mp3");
 let pauseAudio = new Audio("./assets/pausa.mp3")
 
-// header play/pause
+// Header play/pause
 let lofi = document.getElementById('lofi');
 let pause = document.querySelector('.pause');
 let play = document.querySelector('.play');
+
+//To-do-list
+const notesContainer = document.querySelector('.create-tasks');
+const noteInput = document.querySelector('.new-task-input');
+const addNoteBtn = document.querySelector('.new-task-button');
 
 //Eventlisteners
 start.addEventListener('click', pomodoro);
 pause.addEventListener('click', pausar);
 play.addEventListener('click', executar);
+//Eventlisteners closeModal
+closeModal.addEventListener('click', () => {
+    document.querySelector('.modal-tutorial').style.display = 'none';
+});
+//Eventlisteners reiniciar o pomodoro
+btnStart.addEventListener('click', () => {
+    let tutorialStorage = Number(localStorage.getItem('tutorial'));
+    if (tutorialStorage == '') {
+        localStorage.setItem('tutorial', 1);
+    }else {
+        document.querySelector('.modal-tutorial').style.display = 'none';
+    }
+    location.reload();
+});
+//Eventlisteners adicionar to-do-list
+addNoteBtn.addEventListener('click', addNote);
 
+noteInput.addEventListener('keypress', function(e) {
+    if(e.keyCode === 13) {
+        addNote();
+    }
+});
 
+// LocalStorage tutorial
+let tutorialStorage = Number(localStorage.getItem('tutorial'));
+if (tutorialStorage == '') {
+    localStorage.setItem('tutorial', 1);
+}else {
+    document.querySelector('.modal-tutorial').style.display = 'none';
+}
+
+// regex
+let regex = /^[0123456789]+$/;
 
 // Função para pausar a musica tirar o botão pause e colocar o play
 function pausar(){
@@ -60,19 +96,19 @@ function pausar(){
 
 function pomodoro() {
     
-    if (time.value == 0) {
+    if (regex.test(time.value) == false || time.value == 0)  {
         setTimeout(function() {
             time.style.border = "none";
         }, 3000);
             time.style.border = "thick solid #F00";
             time.focus();
-    } else if (pausa.value == 0) {
+    } else if (regex.test(pausa.value) == false || time.value == 0) {
         setTimeout(function() {
             pausa.style.border = "none";
         }, 3000);
             pausa.style.border = "thick solid #F00";
             pausa.focus();
-    } else if (sessoes.value == 0) {
+    } else if (regex.test(sessoes.value) == false || time.value == 0) {
         setTimeout(function() {
             sessoes.style.border = "none";
         }, 3000);
@@ -129,7 +165,7 @@ function count() {
                 clearInterval(secInterval);
 
                 pauseAudio.play();
-                window.navigator.vibrate(600);
+                window.navigator.vibrate(900);
 
                 timePomodoro();
         }
@@ -172,9 +208,11 @@ function timePomodoro() {
 
                 if (ses <= 0) {
                     final.play();
-                    localStorage.clear();
+                    localStorage.removeItem("time");
+                    localStorage.removeItem("pausa");
+                    localStorage.removeItem("sessoes");
                     lofi.pause();
-                    window.navigator.vibrate(600);
+                    window.navigator.vibrate(900);
 
                     timer.style.display = 'none';
                     finalPomodoro.style.display = 'flex';
@@ -187,3 +225,55 @@ function timePomodoro() {
         }
     }, 1000);
 }
+
+function addNote() {
+    if (noteInput.value == '') {
+        setTimeout(function() {
+            noteInput.classList.remove('error');
+        }, 2000);
+        noteInput.classList.add('error');
+    } else {
+        let inputValue = noteInput.value;
+        let getLocalStorage = localStorage.getItem("New Todo");
+        if (getLocalStorage == null) {
+            listArray = []
+        } else {
+            listArray = JSON.parse(getLocalStorage);
+        }
+    
+        listArray.push(inputValue);
+        localStorage.setItem("New Todo", JSON.stringify(listArray));
+        atualizaTasks();
+    }
+}
+
+function atualizaTasks() {
+    let getLocalStorage = localStorage.getItem("New Todo");
+    if (getLocalStorage == null) {
+        listArray = []
+    } else {
+        listArray = JSON.parse(getLocalStorage);
+    }
+    let newTag = '';
+    listArray.forEach((element, index) => {
+        newTag += `<ul class="notes-container">
+        <li class="notesLi">
+            <p class="notesP">${element}</p>
+            <ion-icon name="trash-outline" class="trash" onclick="deleteTask(${index})"></ion-icon>
+        </li>
+    </ul> `
+    });
+
+    notesContainer.innerHTML = newTag;
+    noteInput.value = '';
+}
+
+function deleteTask(index) {
+    let getLocalStorage = localStorage.getItem("New Todo");
+    listArray = JSON.parse(getLocalStorage);
+    listArray.splice(index, 1); 
+    localStorage.setItem("New Todo", JSON.stringify(listArray));
+    atualizaTasks(); 
+  }
+
+atualizaTasks();
